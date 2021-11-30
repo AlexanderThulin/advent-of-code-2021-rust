8dd8c04d3bae8227ad12e43f8e64825083670efe
Alexander Thulin <alexander.t@bokio.se>
2021-11-30 18:56:33 +0100

    feat: add day selection

---
+++ src/days/mod.rs
@@ -0,0 +1,4 @@
+pub mod one;
+pub mod two;
+
+pub mod twentytwenty_one;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod days;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+	let day: u32 = get_day_selection();
+
+	match day {
+		1 => { days::one::run(); }
+		2 => { days::two::run(); }
+		20201 => { days::twentytwenty_one::run() }
+		_ => { println!("Day not started."); }
+	}
+}
+
+fn get_day_selection() -> u32 {
+	let mut day = String::new();
+	println!("Day: ");
+	let _ = std::io::stdin().read_line(&mut day).unwrap();
+	utils::remove_whitespace(&mut day);
+	println!("Selected day {}", day);
+	day.parse::<u32>().expect("Invalid selection.")
 }
